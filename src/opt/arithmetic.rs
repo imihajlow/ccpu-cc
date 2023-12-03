9c8a9c9ce88d60305a5e60b654a59cfaf9802dd9
Ivan Mikhailov <imihajlow@gmail.com>
2023-12-03 22:13:23 +0100

    Some arithmetic optimizations

--- src/opt/arithmetic.rs
+++ src/opt/arithmetic.rs
@@ -13,6 +13,9 @@ pub fn optimize_arithmetics(blocks: &mut Vec<ir::Block>) -> bool {
 fn optimize_op(op: &mut ir::Op) -> bool {
     let new_op = match op {
         ir::Op::Mul(op) => optimize_mul(op),
+        ir::Op::Div(op) => optimize_div(op),
+        ir::Op::Add(op) => optimize_add(op),
+        ir::Op::Sub(op) => optimize_sub(op),
         _ => None,
     };
     if let Some(new_op) = new_op {
@@ -47,6 +50,53 @@ fn optimize_mul(op: &ir::BinaryOp) -> Option<ir::Op> {
             lhs_sign: op.sign,
             lhs_width: op.width,
             rhs: Scalar::ConstInt(log2(*x)),
+        })),
+        _ => None,
+    }
+}
+
+fn optimize_div(op: &ir::BinaryOp) -> Option<ir::Op> {
+    match op.rhs {
+        Scalar::ConstInt(0) => todo!("handle division by zero"),
+        Scalar::ConstInt(1) => Some(ir::Op::Copy(ir::UnaryUnsignedOp {
+            dst: op.dst.clone(),
+            src: op.lhs.clone(),
+            width: op.width,
+        })),
+        Scalar::ConstInt(x) if x.is_power_of_two() => Some(ir::Op::RShift(ir::ShiftOp {
+            dst: op.dst.clone(),
+            lhs: op.lhs.clone(),
+            lhs_sign: op.sign,
+            lhs_width: op.width,
+            rhs: Scalar::ConstInt(log2(x)),
+        })),
+        _ => None,
+    }
+}
+
+fn optimize_add(op: &ir::BinaryOp) -> Option<ir::Op> {
+    let (lhs, rhs) = if let Scalar::ConstInt(_) = op.lhs {
+        (&op.rhs, &op.lhs)
+    } else {
+        (&op.lhs, &op.rhs)
+    };
+
+    match rhs {
+        Scalar::ConstInt(0) => Some(ir::Op::Copy(ir::UnaryUnsignedOp {
+            dst: op.dst.clone(),
+            src: lhs.clone(),
+            width: op.width,
+        })),
+        _ => None,
+    }
+}
+
+fn optimize_sub(op: &ir::BinaryOp) -> Option<ir::Op> {
+    match op.rhs {
+        Scalar::ConstInt(0) => Some(ir::Op::Copy(ir::UnaryUnsignedOp {
+            dst: op.dst.clone(),
+            src: op.lhs.clone(),
+            width: op.width,
         })),
         _ => None,
     }
